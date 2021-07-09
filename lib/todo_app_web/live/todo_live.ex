dc58ed1d430c4fccb76854f8eddabbcbc0e334ec
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2021-07-09 13:21:13 +0200

    Show the todo list

--- lib/todo_app_web/live/todo_live.ex
+++ lib/todo_app_web/live/todo_live.ex
@@ -1,13 +1,24 @@
 defmodule TodoAppWeb.TodoLive do
   use TodoAppWeb, :live_view
 
+  alias TodoApp.Todos
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, fetch(socket)}
   end
 
   def render(assigns) do
     ~L"""
-      <h1>Hello World!</h1>
+      <h1>Todo List</h1>
+      <ul>
+        <%= for todo <- @todos do %>
+          <li><%= todo.title %></li>
+        <% end %>
+      </ul>
     """
+  end
+
+  defp fetch(socket) do
+    assign(socket, %{todos: Todos.list_todos()})
   end
 end
